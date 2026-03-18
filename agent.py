21be147649c3be257632e0555f97aa38dad96c6b
Dawid Boratyński <268094@student.pwr.edu.pl>
2026-03-18 23:23:16 +0100

    Agent update

--- agent.py
+++ agent.py
@@ -3,9 +3,15 @@ import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 import math
+import random
 from game import BBTAN, COLS
 
-def get_rainforce_agent_move(real_game, model, translator_env):
+def get_random_bot_move():
+    random_deg = random.uniform(10, 170)
+    random_angle = -math.pi * random_deg / 180.0
+    return random_angle
+
+def get_reinforce_agent_move(real_game, model, translator_env):
     raw_state = real_game._get_state() 
     obs = translator_env._format_state(raw_state)
     action, _ = model.predict(obs, deterministic=True)
@@ -13,28 +19,21 @@ def get_rainforce_agent_move(real_game, model, translator_env):
     return best_angle
 
 def get_supervised_agent_move(real_game, sl_model):
-    # 1. ZACZYNAMY od Start_X (jeśli w CSV była to pierwsza kolumna)
-    # Kolejność w słowniku musi odpowiadać kolejności w X_train!
     board_state = {'Start_X': real_game.start_x}
     
-    # 2. Dodajemy siatkę klocków R0_C0... R9_C6
     for r in range(10):
         for c in range(7):
             board_state[f'R{r}_C{c}'] = 0
             
-    # 3. Wypełniamy HP klocków
     for b in real_game.blocks:
         if 0 <= b.row < 10 and 0 <= b.col < 7:
             board_state[f'R{b.row}_C{b.col}'] = b.hp
             
-    # 4. Konwersja na DataFrame
     input_df = pd.DataFrame([board_state])
     
-    # 5. KLUCZOWY MOMENT: Upewnij się, że kolumny są w tej samej kolejności co w modelu
-    # sl_model.feature_names_in_ to lista nazw, których oczekuje model
     input_df = input_df[sl_model.feature_names_in_]
     
-    # 6. Predykcja
+    #shot
     predicted_angle = sl_model.predict(input_df)[0]
     
     return predicted_angle
